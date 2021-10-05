feeed7f7b8228bbe11f442731809d3fc85819632
Demix88 <skrasnauskis@gmail.com>
2021-10-05 08:13:14 +0100

    Basic_HTML_and_Javascript_1
    assignments and challanges for HTML  course

---
+++ Basic_JavaScript_2.js
@@ -0,0 +1,7 @@
+
+function multiply(num1,num2){
+let x = Math.floor(Math.random() * 100);
+let y = Math.floor(Math.random() * 100);
+let z = x * y;
+document.getElementById("math1").innerHTML = z;
+}
